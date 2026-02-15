b2edd6320e383e917d9881270a50e6ff505ec36c
rajdeepchaudhari <rajdeepchaudhari.code@gmail.com>
2026-02-15 23:43:30 +0000

    checking update

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,8 +3,26 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://d38793d9bf96ad83bf02ac063ad56605@o4510892035014656.ingest.de.sentry.io/4510892037505104",
 
-  integrations: [Sentry.replayIntegration()],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  replaysOnErrorSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
 });
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
