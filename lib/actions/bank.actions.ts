4ef80ec82609b93f82e0694bcdb2204ec4b0a5fe
rajdeepchaudhari <rajdeepchaudhari.code@gmail.com>
2026-02-15 19:04:23 +0000

    first commit

---
+++ lib/actions/bank.actions.ts
@@ -0,0 +1,193 @@
+"use server";
+
+import {
+  ACHClass,
+  CountryCode,
+  TransferAuthorizationCreateRequest,
+  TransferCreateRequest,
+  TransferNetwork,
+  TransferType,
+} from "plaid";
+
+import { plaidClient } from "../plaid";
+import { parseStringify } from "../utils";
+
+import { getTransactionsByBankId } from "./transaction.actions";
+import { getBanks, getBank } from "./user.actions";
+
+// Get multiple bank accounts
+export const getAccounts = async ({ userId }: getAccountsProps) => {
+  try {
+    // get banks from db
+    const banks = await getBanks({ userId });
+
+    const accounts = await Promise.all(
+      banks?.map(async (bank: Bank) => {
+        // get each account info from plaid
+        const accountsResponse = await plaidClient.accountsGet({
+          access_token: bank.accessToken,
+        });
+        const accountData = accountsResponse.data.accounts[0];
+
+        // get institution info from plaid
+        const institution = await getInstitution({
+          institutionId: accountsResponse.data.item.institution_id!,
+        });
+
+        const account = {
+          id: accountData.account_id,
+          availableBalance: accountData.balances.available!,
+          currentBalance: accountData.balances.current!,
+          institutionId: institution.institution_id,
+          name: accountData.name,
+          officialName: accountData.official_name,
+          mask: accountData.mask!,
+          type: accountData.type as string,
+          subtype: accountData.subtype! as string,
+          appwriteItemId: bank.$id,
+          sharaebleId: bank.shareableId,
+        };
+
+        return account;
+      })
+    );
+
+    const totalBanks = accounts.length;
+    const totalCurrentBalance = accounts.reduce((total, account) => {
+      return total + account.currentBalance;
+    }, 0);
+
+    return parseStringify({ data: accounts, totalBanks, totalCurrentBalance });
+  } catch (error) {
+    console.error("An error occurred while getting the accounts:", error);
+  }
+};
+
+// Get one bank account
+export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
+  try {
+    // get bank from db
+    const bank = await getBank({ documentId: appwriteItemId });
+
+    // get account info from plaid
+    const accountsResponse = await plaidClient.accountsGet({
+      access_token: bank.accessToken,
+    });
+    const accountData = accountsResponse.data.accounts[0];
+
+    // get transfer transactions from appwrite
+    const transferTransactionsData = await getTransactionsByBankId({
+      bankId: bank.$id,
+    });
+
+    const transferTransactions = transferTransactionsData.documents.map(
+      (transferData: Transaction) => ({
+        id: transferData.$id,
+        name: transferData.name!,
+        amount: transferData.amount!,
+        date: transferData.$createdAt,
+        paymentChannel: transferData.channel,
+        category: transferData.category,
+        type: transferData.senderBankId === bank.$id ? "debit" : "credit",
+      })
+    );
+
+    // get institution info from plaid
+    const institution = await getInstitution({
+      institutionId: accountsResponse.data.item.institution_id!,
+    });
+
+    const transactions = await getTransactions({
+      accessToken: bank?.accessToken,
+    });
+
+    const account = {
+      id: accountData.account_id,
+      availableBalance: accountData.balances.available!,
+      currentBalance: accountData.balances.current!,
+      institutionId: institution.institution_id,
+      name: accountData.name,
+      officialName: accountData.official_name,
+      mask: accountData.mask!,
+      type: accountData.type as string,
+      subtype: accountData.subtype! as string,
+      appwriteItemId: bank.$id,
+    };
+
+    // sort transactions by date such that the most recent transaction is first
+    const allTransactions = [...transactions, ...transferTransactions].sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+
+    return parseStringify({
+      data: account,
+      transactions: allTransactions,
+    });
+  } catch (error) {
+    console.error("An error occurred while getting the account:", error);
+  }
+};
+
+// Get bank info
+export const getInstitution = async ({
+  institutionId,
+}: getInstitutionProps) => {
+  try {
+    const institutionResponse = await plaidClient.institutionsGetById({
+      institution_id: institutionId,
+      country_codes: ["US"] as CountryCode[],
+    });
+
+    const intitution = institutionResponse.data.institution;
+
+    return parseStringify(intitution);
+  } catch (error) {
+    console.error("An error occurred while getting the accounts:", error);
+  }
+};
+
+export const getTransactions = async ({
+  accessToken,
+}: getTransactionsProps) => {
+  let hasMore = true;
+  let cursor: string | undefined = undefined;
+  let allTransactions: any[] = [];
+
+  try {
+    while (hasMore) {
+      const response = await plaidClient.transactionsSync({
+        access_token: accessToken,
+        cursor,
+      });
+
+      const data = response.data;
+
+      const newTransactions = data.added.map((t) => ({
+        id: t.transaction_id,
+        name: t.name,
+        paymentChannel: t.payment_channel || "online",
+        type: t.amount > 0 ? "debit" : "credit",
+        accountId: t.account_id,
+        amount: t.amount,
+        pending: t.pending,
+
+        // 🟢 FIXED CATEGORY
+        category:
+          t.personal_finance_category?.primary || t.category?.[0] || "General",
+
+        date: t.date,
+        image: t.logo_url,
+      }));
+
+      allTransactions.push(...newTransactions);
+
+      hasMore = data.has_more;
+      cursor = data.next_cursor;
+    }
+
+    return parseStringify(allTransactions);
+  } catch (error) {
+    console.error("Plaid sync error:", error);
+    return [];
+  }
+};
