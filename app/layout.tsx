e176a9ee6e898f7883072595f10681ebadeecf95
rajdeepchaudhari <rajdeepchaudhari.code@gmail.com>
2026-02-15 19:42:12 +0000

    Logo update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "Cosmico",
+  description: "Cosmico is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
