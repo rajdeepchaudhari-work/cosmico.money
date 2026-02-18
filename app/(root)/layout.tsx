9db81ddf3df10c0eefca0bdea5746be3d61bb93a
rajdeepchaudhari <rajdeepchaudhari.code@gmail.com>
2026-02-18 10:01:39 +0000

    Redirect unauthenticated users to /landing instead of /sign-in

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({
 }>) {
   const loggedIn = await getLoggedInUser();
 
-  if(!loggedIn) redirect('/sign-in')
+  if(!loggedIn) redirect('/landing')
 
   return (
     <main className="flex h-screen w-full font-inter">
